72e390c991776cacfc5634db3e5e72750bad5cf4
sweetbun560935 <colorpen@tutamail.com>
2024-03-15 15:21:32 +0200

    update dependencies

---
+++ js/pag.js
@@ -0,0 +1,16 @@
+$(document).ready(pagination(1));
+
+function pagination(partida){
+	var url = 'db/paginarMascotas.php';
+	$.ajax({
+		type:'POST',
+		url:url,
+		data:'partida='+partida,
+		success:function(data){
+			var array = eval(data);
+			$('#agrega-registros').html(array[0]);
+			$('#pagination').html(array[1]);
+		}
+	});
+	return false;
+}
